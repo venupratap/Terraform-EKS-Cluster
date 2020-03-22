adc0707413f7d4c59d9f24843bb3cc78a7fdc280
mupparajuvamsi <mupparajuvamsi@gmail.com>
2020-03-22 12:40:07 +0530

    terraform

---
+++ Kubernetes/variable.tf
@@ -0,0 +1,32 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "key" {
+  description = "Enter Key name"
+}
+
+variable "sub_ids" {
+  default = []
+}
+
+variable "instance-ami" {
+  default = "ami-0e2ff28bfb72a4e45"
+}
+
+variable "instance_type" {
+  default = "t3.medium"
+}
+
+
+variable "cluster-name" {
+  description = "Cluster Name"
+}
+
+variable "server-name" {
+  description = "Ec2 Server Name"
+}
+
+variable "vpc_name" {
+  description = "VPC name"
+}
